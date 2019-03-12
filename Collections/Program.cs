c1d7a4ac01e2c8313e682e9289d783ea7b022c88
Nathan Gonzalez <ncgonz@gmail.com>
2019-03-11 19:34:39 -0500

    added some dictionary and list examples

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Collections
 {
@@ -6,7 +7,38 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var studentsWithBlackHair = new List<string> { "Nathan", "Austin", "Marty mcfly" };
+
+            var studentsByHairColor = new Dictionary<string, List<string>>
+            {
+                {"Black", studentsWithBlackHair}
+            };
+
+            studentsByHairColor.Add("Bald", new List<string> { "Martin" });
+            studentsByHairColor.Add("Bald", new List<string> { "Adam" });
+
+            var theBlackHairedStudents = studentsByHairColor["Black"];
+
+            if (theBlackHairedStudents == studentsWithBlackHair)
+            {
+                Console.WriteLine("They are the same");
+            }
+
+            studentsByHairColor["Black"].Add("new person");
+
+            foreach (var (hairColor, students) in studentsByHairColor)
+            {
+                Console.WriteLine($"The following students have {hairColor}");
+
+                students.Add("new person");
+
+                foreach (var student in students)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
